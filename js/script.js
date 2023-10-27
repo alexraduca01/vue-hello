1dcd89710dd01018b95bfa1dd5a1fe5b2532b691
alexraduca01 <alex.raduca01@icloud.com>
2023-10-27 15:06:53 +0200

    added style

--- js/script.js
+++ js/script.js
@@ -9,7 +9,10 @@ createApp({
     data() {
         return {
             message: "Kaizoku ou ni ore wa naru!",
-            imgPath: "../img/Rufy_bambino.webp",
+            imgPath: "img/Rufy_bambino.webp",
+            flexClass: "d-flex justify-content-center",
+            h1Classes: "text-center display-1 text-danger fw-bold",
+            bgOnePiece: `background-image: url("img/one-piece-4k-3840x2160-10904.png"); background-size: cover; background-repeat: no-repeat; height: 100vh;`,
         };
     },
 }).mount("#app");
